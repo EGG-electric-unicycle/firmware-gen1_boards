63830c6b733c96a499497e3c0159197e230f0ef8
Jorge Pinto <jpinto@beeverycreative.com>
2015-11-17 03:27:20 -0800

    This code should flash the LEDs from battery indicator

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2015 Joerg Hoener
+    Copyright (C) 2015 Joerg Hoener;
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -16,8 +16,53 @@
 */
 
 #include "stm32f10x.h"
+#include "stm32f10x_gpio.h"
+#include "gpio.h"
+
+void SysTick_Handler(void) // runs every 10ms
+{
+  static unsigned int counter = 0;
+  static unsigned int led_state_flag = 0;
+
+  counter++;
+  if (counter > 100) // 1 second
+  {
+    if (led_state_flag == 0)
+    {
+      /* Enable the LEDs */
+      GPIO_ResetBits(GPIOB, GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_8 | GPIO_Pin_9);
+      GPIO_ResetBits(GPIOA, GPIO_Pin_15);
+
+      led_state_flag = 1;
+    }
+    else
+    {
+      /* Disable the LEDs */
+      GPIO_SetBits(GPIOB, GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_8 | GPIO_Pin_9);
+      GPIO_SetBits(GPIOA, GPIO_Pin_15);
+
+      led_state_flag = 0;
+    }
+
+    counter = 0;
+  }
+}
+
+void initialize (void)
+{
+  gpio_init ();
+  
+  /* Setup SysTick Timer for 10 millisecond interrupts, also enables Systick and Systick-Interrupt */
+  if (SysTick_Config(SystemCoreClock / 100))
+  {
+    /* Capture error */
+    while (1);
+  }
+}
 
 int main(void)
 {
+  initialize();
+
   while (1) ;
 }
