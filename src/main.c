26e9adb6275fe5023145237babc7a89d04e09a8a
casainho <casainho@gmail.com>
2015-12-01 15:02:09 -0800

    Now the clock should be the max possible of 64 MHz

--- src/main.c
+++ src/main.c
@@ -48,13 +48,58 @@ void SysTick_Handler(void) // runs every 10ms
   }
 }
 
+/*
+ * Configure the system clock to be 64 MHz (this is the max clock speed possible using the High Speed Internal oscillator)
+ */
+void SetSysClockTo64(void)
+{
+  /* Enable Prefetch Buffer */
+  FLASH->ACR |= FLASH_ACR_PRFTBE;
+
+  /* Flash 2 wait state */
+  FLASH->ACR &= (uint32_t)((uint32_t)~FLASH_ACR_LATENCY);
+  FLASH->ACR |= (uint32_t)FLASH_ACR_LATENCY_2;
+
+  /* HCLK = SYSCLK */
+  RCC->CFGR |= (uint32_t)RCC_CFGR_HPRE_DIV1;
+
+  /* PCLK2 = HCLK */
+  RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE2_DIV1;
+
+  /* PCLK1 = HCLK */
+  RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE1_DIV2;
+
+  /*  PLL configuration: PLLCLK = HSI/2 * 16 = 64 MHz (this is the max clock speed possible using HSI) */
+  RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_PLLSRC | RCC_CFGR_PLLXTPRE | RCC_CFGR_PLLMULL));
+  RCC->CFGR |= (uint32_t)(RCC_CFGR_PLLSRC_HSI_Div2 | RCC_CFGR_PLLMULL16);
+
+  /* Enable PLL */
+  RCC->CR |= RCC_CR_PLLON;
+
+  /* Wait till PLL is ready */
+  while((RCC->CR & RCC_CR_PLLRDY) == 0)
+  {
+  }
+
+  /* Select PLL as system clock source */
+  RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_SW));
+  RCC->CFGR |= (uint32_t)RCC_CFGR_SW_PLL;
+
+  /* Wait till PLL is used as system clock source */
+  while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) != (uint32_t)0x08)
+  {
+  }
+}
+
 void initialize (void)
 {
+  SetSysClockTo64(); //configure clock to 64 MHz (max possible speed)
+  SystemCoreClockUpdate();
   gpio_init ();
   commutation_disable ();
   pwm_init ();
   hall_sensor_init ();
-  
+
   /* Setup SysTick Timer for 10 millisecond interrupts, also enables Systick and Systick-Interrupt */
   if (SysTick_Config(SystemCoreClock / 100))
   {
@@ -65,6 +110,7 @@ void initialize (void)
 
 int main(void)
 {
+
   initialize();
 
   motor_set_duty_cycle (100); // 100 --> 10%
